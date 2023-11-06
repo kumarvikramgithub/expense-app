9bdd1921818954b503735c7f117dda5e5e2fcefe
Kumar Vikram <vikram2015cse01@gmail.com>
2023-11-06 22:16:58 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="w-screen">
+        <h1 className="text-center text-4xl font-semibold text-purple-500">
+          Hello Frontend
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
